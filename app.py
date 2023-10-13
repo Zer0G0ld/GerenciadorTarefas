6e6c9c411bbf4dac33390fcea92c5a7f9023dee8
Zer0 <114886018+Zer0G0ld@users.noreply.github.com>
2023-10-12 21:20:17 -0300

    Update app.py
    Update this file

--- app.py
+++ app.py
@@ -1,57 +1,67 @@
+# Author: Zer0G0ld
+# Divirtam-se modificando o código !!!
+
 import tkinter as tk
 
-class GerenciadorTarefasGUI:
+class GerenciadorTarefaGUI:
     def __init__(self, root):
         self.root = root
         self.root.title("Gerenciador de Tarefas")
 
-        # Definir tamanho da janela para ocupar toda a tela
-        largura_tela = root.winfo_screenwidth()
-        altura_tela = root.winfo_screenheight()
-        self.root.geometry(f"{largura_tela}x{altura_tela}")
+        # Definir tamanho da janela para ocupar a tela
+        # width_window = root.winfo_screenwidth()
+        # height_window = root.winfo_screenheight()
+        # self.root.geometry(f"{width_window}x{height_window}")
+        self.root.geometry(f"500x500")
 
-        self.tarefas = []
+        self.tasks = []
 
-        self.frame_centro = tk.Frame(root)
-        self.frame_centro.pack(expand=True, fill="both")
+        self.frame_center = tk.Frame(root)
+        self.frame_center.pack(expand=True, fill="both")
 
-        self.input_tarefa = tk.Entry(self.frame_centro)
-        self.input_tarefa.pack(pady=10, padx=20, fill="both")
+        self.input_tasks = tk.Entry(self.frame_center)
+        self.input_tasks.pack(pady=10, padx=20, fill="both")
 
-        self.botao_adicionar = tk.Button(self.frame_centro, text="Adicionar Tarefa", command=self.adicionar_tarefa)
-        self.botao_adicionar.pack(pady=10, fill="both")
+        self.button_add = tk.Button(
+            self.frame_center, text="Adicionar Tarefa", command=self.add_task
+        )
+        self.button_add.pack(pady=10, fill="both")
 
-        self.botao_remover = tk.Button(self.frame_centro, text="Remover Tarefa Selecionada", command=self.remover_tarefa)
-        self.botao_remover.pack(pady=10, fill="both")
+        self.button_remove = tk.Button(
+            self.frame_center, text="Remover Tarefa Selecionada", command=self.remove_task
+        )
+        self.button_remove.pack(pady=10, fill="both")
 
-        self.botao_limpar = tk.Button(self.frame_centro, text="Limpar Tarefas Completas", command=self.limpar_tarefas)
-        self.botao_limpar.pack(pady=10, fill="both")
+        self.button_clear = tk.Button(
+            self.frame_center, text="Limpar Tarefas completas", command=self.clear_tasks
+        )
+        self.button_clear.pack(pady=10, fill="both")
 
-        self.lista_tarefas = tk.Listbox(self.frame_centro)
-        self.lista_tarefas.pack(pady=10, padx=20, fill="both", expand=True)
+        self.list_tasks = tk.Listbox(self.frame_center)
+        self.list_tasks.pack(pady=10, padx=20, fill="both", expand=True)
+    
+    def add_task(self):
+        task = self.input_tasks.get()
+        if task:
+            self.tasks.append(task)
+            self.list_tasks.insert(tk.END, task)
+            self.input_tasks.delete(0, tk.END)
+    
+    def remove_task(self):
+        selected = self.list_tasks.curselection()
+        if selected:
+            index = int(selected[0])
+            self.list_tasks.delete(index)
+            self.tasks.pop(index)
 
-    def adicionar_tarefa(self):
-        tarefa = self.input_tarefa.get()
-        if tarefa:
-            self.tarefas.append(tarefa)
-            self.lista_tarefas.insert(tk.END, tarefa)
-            self.input_tarefa.delete(0, tk.END)
-
-    def remover_tarefa(self):
-        selecionado = self.lista_tarefas.curselection()
-        if selecionado:
-            indice = selecionado[0]
-            self.lista_tarefas.delete(indice)
-            self.tarefas.pop(indice)
-
-    def limpar_tarefas(self):
-        tarefas_completas = [tarefa for tarefa in self.tarefas if tarefa]
-        self.lista_tarefas.delete(0, tk.END)
-        self.tarefas = []
-        for tarefa in tarefas_completas:
-            self.lista_tarefas.insert(tk.END, tarefa)
+    def clear_tasks(self):
+        tasks_complete = [task for task in self.tasks if task]
+        self.list_tasks.delete(0, tk.END)
+        self.tasks = []
+        for task in tasks_complete:
+            self.list_tasks.insert(tk.END, task)
 
 if __name__ == "__main__":
     root = tk.Tk()
-    app = GerenciadorTarefasGUI(root)
+    app = GerenciadorTarefaGUI(root)
     root.mainloop()
